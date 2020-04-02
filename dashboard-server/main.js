36bf4ea89031f49cbaf3d34407dac199b187c1e3
benlowry <benlowry@outlook.com>
2020-04-02 10:01:01 -0600

    Leaner startup

--- dashboard-server/main.js
+++ dashboard-server/main.js
@@ -1,15 +1,2 @@
 const dashboard = require('@userdashboard/dashboard')
-
-module.exports = {
-  start: (dirname) => {
-    global.applicationPath = dirname || __dirname
-    dashboard.start()
-    delete (global.sitemap['/public/home.svg'])
-    delete (global.sitemap['/public/extended.svg'])
-  },
-  stop: () => {
-    dashboard.stop()
-  }
-}
-
-module.exports.start(__dirname)
+dashboard.start(__dirname)
