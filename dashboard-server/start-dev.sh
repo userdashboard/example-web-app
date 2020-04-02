e4cb7eaf16fabab7c53b67a336c176fa60a8e671
benlowry <benlowry@outlook.com>
2020-04-02 12:30:09 -0600

    Removed domain var since it runs on localhost

--- dashboard-server/start-dev.sh
+++ dashboard-server/start-dev.sh
@@ -1,9 +1,8 @@
 cd "$(dirname "$0")"
 
 APPLICATION_SERVER_TOKEN="this is the token" \
-APPLICATION_SERVER="http://localhost:8200" \
+APPLICATION_SERVER="http://localhost:8213" \
 DASHBOARD_SERVER="http://localhost:8207" \
-DOMAIN="localhost:8207" \
 PORT=8207 \
 IP=0.0.0.0 \
 GENERATE_API_TXT=false \
