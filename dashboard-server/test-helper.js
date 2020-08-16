1e9d44f21ef6f4109bbc465bfa69b9ea86617e1c
benlowry <benlowry@outlook.com>
2020-08-16 11:13:56 -0600

    Switch to global container for test configuration

--- dashboard-server/test-helper.js
+++ dashboard-server/test-helper.js
@@ -1,23 +1,11 @@
 /* eslint-env mocha */
-let applicationServer
+global.applicationPath = __dirname
+global.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
+global.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
+global.defaultConfiguration.applicationServerToken = 'token'
 
-const TestHelper = module.exports = require('@userdashboard/organizations/test-helper.js')
-TestHelper.defaultConfiguration.applicationServer = `http://localhost:${process.env.APPLICATION_SERVER_PORT}`
-TestHelper.defaultConfiguration.applicationServerPort = process.env.APPLICATION_SERVER_PORT
-TestHelper.defaultConfiguration.applicationServerToken = 'token'
+module.exports = require('@userdashboard/organizations/test-helper.js')
 
-before(async () => {
-  if (applicationServer) {
-    return
-  }
-  applicationServer = require('../application-server/main.js')
-  await applicationServer.start(process.env.APPLICATION_SERVER_PORT)
-})
-
-
-after(async () => {
-  if (applicationServer) {
-    await applicationServer.stop()
-    applicationServer = null
-  }
-})
+const applicationServer = require('../application-server/main.js')
+applicationServer.start(process.env.APPLICATION_SERVER_PORT, global.dashboardServer)
+after(applicationServer.stop)
